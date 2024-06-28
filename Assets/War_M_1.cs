92250fd8d706c1464ba78160a905a1dfdbb20d3f
Vishal Davra <56792574+vishaldavra@users.noreply.github.com>
2024-06-28 14:32:40 +0530

    updates

--- Assets/War_M_1.cs
+++ Assets/War_M_1.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class War_M_1 : MonoBehaviour
 {
-    public Transform turretTransform; // Assign this in the Inspector to the turret part of your tank
+    public Transform turretTransform; 
+    public Transform Barreltransform; 
    
     public float rotationSpeed;
     void Update()
@@ -14,11 +15,16 @@ public class War_M_1 : MonoBehaviour
         {
             // Calculate direction from turret to player
             Vector3 targetDirection = LevelManager.instance.LiveBall.transform.GetChild(0).transform.position - turretTransform.position;
+            Vector3 targetDirection2 = LevelManager.instance.LiveBall.transform.GetChild(0).transform.position - turretTransform.position;
             targetDirection.y = 0f; // Ensure turret only rotates around the Y-axis
 
             // Rotate turret towards the player
             Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+           
+            Quaternion targetRotation2 = Quaternion.LookRotation(targetDirection2);
+            targetRotation2.z = 0f;
             turretTransform.rotation = Quaternion.RotateTowards(turretTransform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            Barreltransform.rotation = Quaternion.RotateTowards(Barreltransform.rotation, targetRotation2, Time.deltaTime * rotationSpeed);
         }
 
       
